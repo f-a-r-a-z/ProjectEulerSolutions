491ebcb1e79aadecf6e0c0dc72d23af2a48e49e8
f-a-r-a-z <farazshafiq99+f-a-r-a-z@gmail.com>
2019-01-17 21:43:28 +1000

    Remove unused using directives

--- ProjectEulerSolutions/ProjectEulerSolutions/Program.cs
+++ ProjectEulerSolutions/ProjectEulerSolutions/Program.cs
@@ -1,12 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectEulerSolutions {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine(Problems.Problem001.Solution());
+            Console.ReadLine();
         }
     }
 }
