91750b4decbda6dba0e8abee120693f5870571d4
f-a-r-a-z <farazshafiq99+f-a-r-a-z@gmail.com>
2019-01-17 17:20:57 +1000

    Add problem template

---
+++ ProjectEulerSolutions/ProjectEulerSolutions/Problems/ProblemTemplate.cs
@@ -0,0 +1,12 @@
+﻿/*
+    Problem Description
+    https://projecteuler.net/problem=N
+*/
+
+namespace ProjectEulerSolutions.Problems {
+    class ProblemTemplate {
+        public static int Solution() {
+            return 0;
+        }
+    }
+}
