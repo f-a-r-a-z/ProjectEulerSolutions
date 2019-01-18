4dff14d0040a3e6086706588cdc4e91816a24e27
f-a-r-a-z <farazshafiq99+f-a-r-a-z@gmail.com>
2019-01-18 13:45:36 +1000

    Add solution to problem 5

---
+++ ProjectEulerSolutions/Problems/Problem005.cs
@@ -0,0 +1,21 @@
+﻿/*
+    What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+    https://projecteuler.net/problem=5
+*/
+
+namespace ProjectEulerSolutions.Problems {
+    class Problem005 {
+        public static int Solution() {
+            int curr = 20;
+
+            for (; ;) {
+                for (int i = 2; i <= 20; i++) {
+                    if (curr % i != 0) break;
+                    if (i == 20) return curr;
+                }
+
+                curr += 20;
+            }
+        }
+    }
+}
--- ProjectEulerSolutions/Program.cs
+++ ProjectEulerSolutions/Program.cs
@@ -7,6 +7,8 @@ namespace ProjectEulerSolutions {
             Console.WriteLine("Problem 2 Solution: " + Problems.Problem002.Solution());
             Console.WriteLine("Problem 3 Solution: " + Problems.Problem003.Solution());
             Console.WriteLine("Problem 4 Solution: " + Problems.Problem004.Solution());
+            Console.WriteLine("Problem 5 Solution: " + Problems.Problem005.Solution());
+            Console.Write("Press Enter to exit");
             Console.ReadLine();
         }
     }
